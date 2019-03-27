bfdcb5141e3f3c45cae5cfcbd5d224776c0e97db
Sebastien Thibert <sebastien.thibert@marks-and-spencer.com>
2019-03-27 13:26:56 +0000

    Filter offer ids
    Compare a products offer ids to the offer ids available to the user.
    
    Remove any product offer ids that don't match a users offer ids.
    
    Build a new array of products with only valid user offer ids.

--- RetailApp/ProductListing/ProductListingViewModel.swift
+++ RetailApp/ProductListing/ProductListingViewModel.swift
@@ -12,13 +12,23 @@ class ProductListingViewModel {
 
   func getProducts(completion: @escaping () -> Void) {
     service.getProductListing { [weak self] result in
+      guard let self = self else { return }
       do {
-        self?.products = try result.unwrapped().products
+        self.products = try self.filterProductOfferIdsWithUserOfferIds(result.unwrapped().products, user: self.user)
         completion()
       } catch {
         print(error.localizedDescription)
       }
     }
+  }
+
+  private func filterProductOfferIdsWithUserOfferIds(_ products: [ProductListingProduct], user: User) -> [ProductListingProduct] {
+    var filteredProducts: [ProductListingProduct] = []
+    products.forEach {
+      let offerIds = $0.offerIds.filter { user.offers.map { $0.id }.contains($0) }
+      filteredProducts.append(ProductListingProduct(id: $0.id, imageKey: $0.imageKey, name: $0.name, offerIds: offerIds, price: $0.price))
+    }
+    return filteredProducts
   }
 }
 
